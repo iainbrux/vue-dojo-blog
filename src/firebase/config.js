586399717576e7e7a9094ef7464981d10af8014c
iainbrux <me@iainbrux.com>
2021-05-22 17:33:09 +0100

    initialise firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUuxumyPEgtPEjuTXkFd_LP7e6x1EfGYQ",
+  authDomain: "netninja-vue-projects.firebaseapp.com",
+  projectId: "netninja-vue-projects",
+  storageBucket: "netninja-vue-projects.appspot.com",
+  messagingSenderId: "14958471113",
+  appId: "1:14958471113:web:a32bc61b667faf315faa18",
+};
+
+//initialise firebase
+firebase.initializeApp(firebaseConfig);
+
+//initialise firestore service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
